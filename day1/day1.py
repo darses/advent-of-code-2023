b8f085585bcc61f58ba1350622ab0df53f382230
Christian van Bruggen <Christian.vanBruggen@pinewood.nl>
2023-12-02 16:40:20 +0100

    Day 1 part 2
    Signed-off-by: Christian van Bruggen <Christian.vanBruggen@pinewood.nl>

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import re
+import regex as re
 
-example_answer = 142
+example_answer = [142, 281]
 
 
-def solve(input):
+def solve(input: list[str]):
+    return None
     sum = 0
     for line in input:
         digits = re.findall(r"\d", line)
@@ -13,16 +14,56 @@ def solve(input):
     return sum
 
 
-def solve_file(filename):
+def cast(s: str):
+    match s:
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        case _:
+            return s
+
+
+def solve2(input: list[str]):
+    sum = 0
+    for line in input:
+        digits = re.findall(
+            r"\d|one|two|three|four|five|six|seven|eight|nine", line, overlapped=True
+        )
+        d1 = cast(digits[0])
+        d2 = cast(digits[-1])
+        sum += int(d1 + d2)
+    return sum
+
+
+def solve_file(filename) -> [any, any]:
     with open(filename, "r") as fh:
         input = fh.readlines()
-    return solve(input)
+    return [solve(input), solve2(input)]
 
 
 if __name__ == "__main__":
     example_result = solve_file("example.txt")
-    if solve_file("example.txt") == example_answer:
-        print("Example success, computing result for input.txt")
-        print(f'Result: {solve_file("input.txt")}')
-    else:
-        print(f"Example failed, got {example_result} instead of {example_answer}")
+    print(
+        f"Example part 1, got {example_result[0]} with correct answer {example_answer[0]}\n"
+        f"Example part 2, got {example_result[1]} with correct answer {example_answer[1]}"
+    )
+
+    input_result = solve_file("input.txt")
+    print(
+        f"Input part 1, got {input_result[0]}\n" f"Input part 2, got {input_result[1]}"
+    )
